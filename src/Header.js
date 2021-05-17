88acffbd7e6c77bbe9a1b19ca23cffe55133d113
LuisMrD <luismrd@localhost.localdomain>
2021-05-16 21:09:31 -0300

    header component added

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <i class="fas fa-book fa-2x"></i>
+            <h1>Search Books</h1>
+        </header>
+    )
+}
+
+export default Header;
