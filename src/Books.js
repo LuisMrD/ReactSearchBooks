5275b6d0364002014fecd6bf83019a562ff07e39
LuisMrD <luismrd@localhost.localdomain>
2021-05-17 06:45:56 -0300

    superagent installed

--- src/Books.js
+++ src/Books.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import SearchArea from './SearchArea';
+import request from 'superagent';
 
 class Books extends Component {
     constructor(props){
@@ -10,8 +11,16 @@ class Books extends Component {
         }
     }
 
+    searchBook = () => {
+        request
+            .get("https://www.googleapis.com/books/v1/volumes")
+            .query({q: this.searchField})
+            .then((data) => {
+                console.log(data);
+            })
+    }
+
     handleSearch = (e) => {
-        console.log(e.target.value)
         this.setState({ searchField: e.target.value})
     }
 
