e6a3aeb66e05e8568b8278864aaf0dc5f93c0c30
LuisMrD <luismrd@localhost.localdomain>
2021-05-17 16:33:53 -0300

    saving favorite books in localstorage

--- src/BookCard.js
+++ src/BookCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+function addFavBook(image, title, date, desc, link){
+    var bookInfos = {image: image, title: title, date: date, desc: desc, link: link}
+    localStorage.setItem(link, JSON.stringify(bookInfos));
+}
+
 const BookCard = (props) => {
     return (
         <div className="card-container">
-            <a href={props.link} target="_blank"><img src={props.image}/></a>
+            <a href={props.link} target="_blank" rel="noreferrer"><img src={props.image} alt=""/></a>
             <div className="desc">
                 <h2>Título: {props.title}</h2>
                 <h3>Ano: {props.published}</h3>
                 <p>Descrição: {props.description}</p>
+                <input onClick={() => addFavBook(props.image, props.title, props.published, props.description, props.link)} type="checkbox" />
             </div>
         </div>
     )
---
+++ src/BookFavList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import BookCard from './BookCard';
+
+const BookList = (props) => {
+    return (
+        <div className="list">
+            {
+                props.books.map((book, i) => {
+                    var thumb = book.volumeInfo.imageLinks;
+                    if(typeof thumb != 'undefined'){
+                        thumb = book.volumeInfo.imageLinks.thumbnail;
+                    }else {
+                        //thumb = 'https://a-static.mlcdn.com.br/574x431/livro-mal-da-terra/ludylivros/9788577530052/6f32dc7781463b4e1dd866ef1c1cbe9b.jpg';
+                        thumb = 'https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/822a5d14da9a2c8b035750e0f1838e2c186360a9.jpg';
+                    }
+
+                    var desc = book.volumeInfo.description;
+                    if(typeof desc != 'undefined'){
+                        desc = book.volumeInfo.description;
+                    }else {
+                        desc = 'Sem descrição disponível';
+                    }
+
+                    var pubDay = book.volumeInfo.publishedDate
+                    if(typeof pubDay != 'undefined'){
+                        pubDay = book.volumeInfo.publishedDate.substring(0,4);
+                    }else {
+                        pubDay = 'Indisponível';
+                    }
+
+                    return <BookCard
+                                key={i} 
+                                image= {thumb}
+                                title={book.volumeInfo.title}
+                                published={pubDay}
+                                description={desc}
+                                link={book.volumeInfo.infoLink}                               
+                            />
+                })
+            }
+        </div>
+    )
+}
+
+export default BookList;
+
+//infoLink
