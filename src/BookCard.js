7edde932a63538513f1bb3c0de1077e81bcae99f
LuisMrD <luismrd@localhost.localdomain>
2021-05-17 14:27:32 -0300

    books info and infoLink on Booklist working

--- src/BookCard.js
+++ src/BookCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 const BookCard = (props) => {
     return (
         <div className="card-container">
-            <img src={props.image} alt=""/>
+            <a href={props.link} target="_blank"><img src={props.image}/></a>
             <div className="desc">
-                <h2>{props.title}</h2>
-                <h3>{props.author}</h3>
-                <p>{props.published}</p>
+                <h2>Título: {props.title}</h2>
+                <h3>Ano: {props.published}</h3>
+                <p>Descrição: {props.description}</p>
             </div>
         </div>
     )
--- src/BookList.js
+++ src/BookList.js
@@ -10,14 +10,31 @@ const BookList = (props) => {
                     if(typeof thumb != 'undefined'){
                         thumb = book.volumeInfo.imageLinks.thumbnail;
                     }else {
-                        thumb = '/googlebooks/images/no_cover_thumb.gif';
+                        //thumb = 'https://a-static.mlcdn.com.br/574x431/livro-mal-da-terra/ludylivros/9788577530052/6f32dc7781463b4e1dd866ef1c1cbe9b.jpg';
+                        thumb = 'https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/822a5d14da9a2c8b035750e0f1838e2c186360a9.jpg';
                     }
+
+                    var desc = book.volumeInfo.description;
+                    if(typeof desc != 'undefined'){
+                        desc = book.volumeInfo.description;
+                    }else {
+                        desc = 'Sem descrição disponível';
+                    }
+
+                    var pubDay = book.volumeInfo.publishedDate
+                    if(typeof pubDay != 'undefined'){
+                        pubDay = book.volumeInfo.publishedDate.substring(0,4);
+                    }else {
+                        pubDay = 'Indisponível';
+                    }
+
                     return <BookCard
                                 key={i} 
                                 image= {thumb}
                                 title={book.volumeInfo.title}
-                                author={book.volumeInfo.authors}
-                                published={book.volumeInfo.publishedDay}
+                                published={pubDay}
+                                description={desc}
+                                link={book.volumeInfo.infoLink}                               
                             />
                 })
             }
@@ -26,3 +43,5 @@ const BookList = (props) => {
 }
 
 export default BookList;
+
+//infoLink
